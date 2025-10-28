6b4b138997219390a252bd5249afa5af874a8866
shahzaib786ahmed <ahmedshahzaib203@gmail.com>
2025-10-28 11:35:10 -0400

    Update routes.php

--- app/Routes/routes.php
+++ app/Routes/routes.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\Controllers\AboutController;
 use App\Controllers\BreweriesController;
+use App\Controllers\LocationsController;
 use App\Helpers\DateTimeHelper;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
