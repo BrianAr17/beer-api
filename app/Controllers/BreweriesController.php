b9b71ec7b18ac5b22cf74a58cb94b1384c1dc7a8
shahzaib786ahmed <ahmedshahzaib203@gmail.com>
2025-10-29 10:28:03 -0400

    Implemented Input Validation

--- app/Controllers/BreweriesController.php
+++ app/Controllers/BreweriesController.php
@@ -4,10 +4,11 @@ namespace App\Controllers;
 
 use App\Domain\Models\BreweriesModel;
 use App\Exceptions\HttpBadRequestException;
+use App\Exceptions\HttpInvalidStringException;
 use App\Exceptions\HttpNotFoundException;
 use App\Exceptions\HttpInvalidNumberException;
+use App\Validation\ValidationHelper;
 use App\Exceptions\HttpRangeValidationException;
-use App\Exceptions\HttpDateFormatException;
 use App\Exceptions\HttpNotFoundException as ExceptionsHttpNotFoundException;
 use InvalidArgumentException;
 use PDO;
@@ -22,6 +23,42 @@ class BreweriesController extends BaseController
     public function handleGetBreweries(Request $request, Response $response): Response
     {
         $filters = $request->getQueryParams();
+
+        $name = $filters['name'] ?? null;
+        $country = $filters['country'] ?? null;
+        $city = $filters['city'] ??  null;
+        $state = $filters['state'] ??  null;
+        $owner_name = $filters['owner_name'] ??  null;
+        $founded_year = $filters['founded_year'] ?? null;
+        $employee_count = $filters['employee_count'] ?? null;
+
+        if ($name !== null && !ValidationHelper::isAlpha($name)) {
+            throw new HttpInvalidStringException($request);
+        }
+
+        if ($country !== null && !ValidationHelper::isAlpha($country)) {
+            throw new HttpInvalidStringException($request);
+        }
+
+        if ($city !== null && !ValidationHelper::isAlpha($city)) {
+            throw new HttpInvalidStringException($request);
+        }
+
+        if ($state !== null && !ValidationHelper::isAlpha($state)) {
+            throw new HttpInvalidStringException($request);
+        }
+
+        if ($owner_name !== null && !ValidationHelper::isAlpha($owner_name)) {
+            throw new HttpInvalidStringException($request);
+        }
+
+        if ($founded_year !== null && !ValidationHelper::isInt($founded_year)) {
+            throw new HttpInvalidNumberException($request);
+        }
+
+        if ($employee_count !== null && !ValidationHelper::isInt($employee_count)) {
+            throw new HttpInvalidNumberException($request);
+        }
 
         $sortBy = $filters['sort_by'] ?? 'brewery_id';
         $order  = strtolower($filters['order'] ?? 'asc');
@@ -60,38 +97,22 @@ class BreweriesController extends BaseController
         //* 1) Get the received ID from the URI.
         $brewery_id = $uri_args["brewery_id"];
 
-        if ($brewery_id <= 0) {
+        if ($brewery_id <= 0 || !ValidationHelper::isInt($brewery_id)) {
             throw new HttpInvalidNumberException($request, 'Invalid Brewery ID: must be a positive integer');
         }
 
+
         //* 2) Fetch the brewery info from the DB by ID.
         $brewery = $this->breweries_model->getBreweryById($brewery_id);
-
-        //* 3) Prepare and return a JSON response.
-        //return $this->renderJson($response, $brewery);
 
 
         if(!$brewery) {
             throw new HttpNotFoundException($request);
         }
 
-        if ($brewery === false) {
+        $response->getBody()->write(json_encode($brewery));
+        return $response->withHeader('Content-Type', 'application/json');
 
-            //? Alternative PATH
-            //? Option #2: Create a well-structured
-            $payload = [
-                "status" => "error",
-                "code" => 404,
-                "message" => "There was no record matching the supplied brewery id..."
-            ];
-            return $this->renderJson($response, $payload, 404);
-        }
-
-        //? Happy PATH:
-        return $this->renderJson($response, $brewery);
-
-        //! 4) What if the ID was invalid?
-        //? Send a very-well structured JSON error response
     }
      /// End of the callback
 
